e45585dd24701de4482f226cccd11ea578c0317c
= <abhisingh30800@gmail.com>
2024-03-18 18:32:50 +0530

    completed making form with post data

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import path from "path";
 
 const app = express();
+const users = [];
 
+// this is also a middleware
 app.use(express.static(path.join(path.resolve(), "public")));
+// middlewares to access the data submitted by the form
+app.use(urlencoded({ extended: true }));
 
 // setting up view engine
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.json({
-    page: "homePage",
-    success: true,
-    status: 200,
-  });
+  res.render("index");
 });
 
-// app.get("/send", (req, res) => {
-//   // const filePath = path.resolve() + "/index.html";
-//   res.sendFile(path.join(path.resolve(), "/index.html"));
-// });
+app.get("/success", (req, res) => {
+  res.render("success");
+});
 
-app.get("/send", (req, res) => {
-  res.render("index", { name: "Abhishek" });
-  // res.send("index");
+app.get("/users", (req, res) => {
+  res.json({ users });
+});
+
+// Post Routes
+app.post("/contact", (req, res) => {
+  users.push({ username: req.body.name, email: req.body.email });
+  res.redirect("/success");
 });
 
 app.listen(5000, () => {
-  console.log("server is working");
+  console.log("server is running");
 });
