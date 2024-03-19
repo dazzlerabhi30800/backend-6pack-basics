6f224c85a99fdec41503db55208947e27918579f
= <abhisingh30800@gmail.com>
2024-03-19 12:53:05 +0530

    added mongoose & create document functionality

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 import express, { urlencoded } from "express";
 import path from "path";
+import mongoose from "mongoose";
 
 const app = express();
 const users = [];
+
+mongoose
+  .connect("mongodb://localhost:27017", {
+    dbName: "backend",
+  })
+  .then((c) => console.log("Database Connect"))
+  .catch((err) => console.log(err));
+
+const messageSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+});
+
+const Message = mongoose.model("Message", messageSchema);
 
 // this is also a middleware
 app.use(express.static(path.join(path.resolve(), "public")));
@@ -27,6 +42,12 @@ app.get("/users", (req, res) => {
 // Post Routes
 app.post("/contact", (req, res) => {
   users.push({ username: req.body.name, email: req.body.email });
+  res.redirect("/success");
+});
+
+app.post("/add", async (req, res) => {
+  const { name, email } = req.body;
+  await Message.create({ name, email });
   res.redirect("/success");
 });
 
