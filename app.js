8f3d861b79c549b430ddec3c26889ec3bba641d4
= <abhisingh30800@gmail.com>
2024-03-26 13:13:26 +0530

    changed origin url #3

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "*",
+    origin: [process.env.FRONTEND_URL.toString()],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
