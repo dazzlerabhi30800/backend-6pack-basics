2057121d51b578fe2d4da18fbdce9fbf71ab0c80
= <abhisingh30800@gmail.com>
2024-03-26 18:35:00 +0530

    changed cors settings

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(
     origin: [process.env.FRONTEND_URL?.toString()],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
+    exposedHeaders: ["Set-Cookie"]
   }),
 );
 
